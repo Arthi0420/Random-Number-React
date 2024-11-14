900167e2f4885d2f4ed6a86cfdd07425e48ec0eb
Arthi0420 <arthi20112003@gmail.com>
2024-11-14 15:57:47 +0530

    Updated Projects

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,26 @@
+
+import { useState } from "react"
+
+function RandomNumber() {
+    var [random, setrandom] = useState(0)
+  
+    function randomnumber() {
+      setrandom(Math.floor(Math.random() * 10) + 1)
+    }
+  
+    return (
+      <>
+        <div className="randomNumber">
+          <div className="container">
+            <h1>Random Number Generator App</h1>
+            <h2>{random}</h2>
+            <div>
+              <button className="btn" onClick={randomnumber}>Get Random Number</button>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  export default RandomNumber
+  
